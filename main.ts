34fea17f0ca14fa7ce8b6ba93b42d94d404d346d
lewfer <34630995+lewfer@users.noreply.github.com>
2022-11-03 16:55:53 +0000

    initial

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
-function sendEnergy (wind: number, solar: number) {
-    serial.writeLine("#" + wind + "," + solar + "," + (wind + solar) + ".")
+//% color="#ff7f50" icon="\uf2f1" block="Energy"
+namespace renewableEnergy {
+
+    //% blockId=getEnergy
+    //% block="get energy from pin %pin"
+    export function getEnergy(pin: AnalogPin) {
+        return pins.analogReadPin(pin)
+    }
+
+    //% blockId=sendEnergy
+    //% block="send energy wind %wind solar %solar to grid"
+    export function sendEnergy (wind: number, solar: number) {
+        serial.writeLine("#" + wind + "," + solar + "," + (wind + solar) + ".")
+    }
+
+    //% blockId=useEnergy
+    //% block="user energy for device %deviceName on pin %pin"
+    export function useEnergy(pin: AnalogPin, deviceName: string) {
+        
+    }
+
+    //% blockId=addDevice
+    //% block="add device %name using power %power"
+    export function addDevice (name: string, power: number) {
+        
+    }
+
 }
-function useEnergy (pin: number, deviceName: string) {
-	
-}
-function addDevice (name: string, watts: number) {
-	
-}
-function getEnergy (pin: number) {
-    return pins.analogReadPin(AnalogPin.P0)
-}
-basic.forever(function () {
-	
-})
