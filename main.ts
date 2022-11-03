300324e27c7f85946287ede3a095dfe86f2c1a2e
lewfer <34630995+lewfer@users.noreply.github.com>
2022-11-03 17:16:21 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,82 @@
-//% color="#ff7f50" icon="\uf2f1" block="Energy"
 namespace renewableEnergy {
 
     //% blockId=getEnergy
     //% block="get energy from pin %pin"
+    //% group="Power Station"
+    //% weight=50
     export function getEnergy(pin: AnalogPin) {
-        return pins.analogReadPin(pin)
+        let energy 
+        energy = pins.analogReadPin(pin)
+        if (energy>maxEnergy)
+            energy = 200
+        return energy
     }
 
     //% blockId=sendEnergy
-    //% block="send energy wind %wind solar %solar to grid"
+    //% block="send energy wind %wind solar %solar to grid"    
+    //% group="Power Station"
+    //% weight=40
     export function sendEnergy (wind: number, solar: number) {
         serial.writeLine("#" + wind + "," + solar + "," + (wind + solar) + ".")
     }
 
+    //% blockId=panelMoveLeft
+    //% block="panel move left"    
+    //% group="Power Station"
+    //% weight=30
+    export function panelMoveLeft() {
+        if (lrAngle > servoMoveIncrement)
+            lrAngle -= servoMoveIncrement
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, lrAngle)
+    }
+
+    //% blockId=panelMoveRight
+    //% block="panel move right"    
+    //% group="Power Station"
+    //% weight=29
+    export function panelMoveRight() {
+        if (lrAngle < 180-servoMoveIncrement)
+            lrAngle += servoMoveIncrement
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, lrAngle)
+    }
+
+    //% blockId=panelMoveUp
+    //% block="panel move up"    
+    //% group="Power Station"
+    //% weight=28
+    export function panelMoveUp() {
+        if (udAngle > servoMoveIncrement)
+            udAngle -= servoMoveIncrement
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, udAngle)
+    }
+
+    //% blockId=panelMoveDown
+    //% block="panel move down"    
+    //% group="Power Station"
+    //% weight=27
+    export function panelMoveDown() {
+        if (udAngle < 180-servoMoveIncrement)
+            udAngle += servoMoveIncrement
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, udAngle)
+    }
+
     //% blockId=useEnergy
     //% block="user energy for device %deviceName on pin %pin"
+    //% group="House"
     export function useEnergy(pin: AnalogPin, deviceName: string) {
         
     }
 
     //% blockId=addDevice
     //% block="add device %name using power %power"
+    //% group="House"
     export function addDevice (name: string, power: number) {
         
     }
 
+    // Globals
+    let maxEnergy = 200
+    let lrAngle = 90
+    let udAngle = 90
+    let servoMoveIncrement = 20
 }
